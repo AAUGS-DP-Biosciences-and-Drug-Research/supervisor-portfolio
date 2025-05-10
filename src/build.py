c86fdb5aef6110b2e0bc5e546ae26bad70975a52
guijacquemet <guillaume.jacquemet@utu.fi>
2025-05-10 08:50:04 +0300

    Update build.py

--- src/build.py
+++ src/build.py
@@ -7,7 +7,7 @@ from jinja2 import Environment, FileSystemLoader
 INPUT_CSV = "data/responses.csv"
 PUBLIC_FOLDER = "public"
 IMAGES_FOLDER = os.path.join(PUBLIC_FOLDER, "images")
-DEFAULT_LOGO = "https://www.abo.fi/wp-content/uploads/2019/09/AboAkademiUniversity.png"
+DEFAULT_LOGO = "AboAkademiUniversity.png"
 
 # ---------- Helpers ----------
 def slugify(name):
